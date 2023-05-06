49717844fb8d4a6502f4dc10d7c56765937fc273
dj0le <gstar12@hotmail.com>
2023-05-06 18:14:16 +0200

    blog layout updates

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -1,26 +1,54 @@
 <script lang="ts">
 	import * as config from '$lib/config'
 	import Toggle from './toggle.svelte'
+
+	let isMenuOpen = false
+
+	function toggleMenu() {
+		isMenuOpen = !isMenuOpen
+	}
+
+	let current = 0
 </script>
 
 <nav>
 	<div class="logo">
 		<img src="/favicon.ico" alt="logo" />
-		<a href="/" class="title"> <b>{config.title}</b></a>
+		<a href="/" on:click={() => (current = 1)} class="title"><b>{config.title}</b></a>
 	</div>
 
 	<ul class="links">
 		<li>
-			<a href="/">portfolio</a>
+			<a
+				class:current={current === 1}
+				on:click={() => (current = 1)}
+				href="/"
+				data-sveltekit-preload-data>portfolio</a
+			>
 		</li>
 		<li>
-			<a href="/blog">blog</a>
+			<a
+				class:current={current === 2}
+				on:click={() => (current = 2)}
+				href="/blog"
+				data-sveltekit-preload-data>blog</a
+			>
 		</li>
 		<li>
-			<a href="/resume">resume</a>
+			<a
+				class:current={current === 3}
+				on:click={() => (current = 3)}
+				href="/resume"
+				data-sveltekit-preload-data>resume</a
+			>
 		</li>
 		<li>
-			<a href="/contact">contact</a>
+			<a
+				class:current={current === 4}
+				on:click={() => (current = 4)}
+				href="/contact"
+				data-sveltekit-preload-data>contact</a
+			>
 		</li>
 	</ul>
 
@@ -58,5 +86,18 @@
 			gap: var(--size-7);
 			margin-block: 0;
 		}
+	}
+	.current {
+		position: relative;
+	}
+
+	.current::after {
+		content: '';
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		width: 100%;
+		height: 2px;
+		background-color: var(--brand);
 	}
 </style>
