ac952b7cf3a4e99a0f13ee5fcbba1b27f9fb7093
dj0le <gstar12@hotmail.com>
2023-06-29 12:01:57 +0200

    portfolio section updates

--- src/lib/components/projects.svelte
+++ src/lib/components/projects.svelte
@@ -4,35 +4,44 @@
 <section>
 	<div class="card">
 		<div class="column">
-			<h3>ToothFairy B2C</h3>
-			<p>full stack ecommerce solution</p>
-			<a href="https://toothfairybh.com">Visit Page</a>
-			<p>Solutions</p>
+			<h3>ToothFairy</h3>
+			<p>
+				Full stack implementation of ecommerce solution. I designed the entire site and have
+				maintained it for 3+ years. It receives 10-15 thousand unique visits per month. 100% Google
+				Lighthouse / Page Speed Insight scores. Focus on SEO best practices and accessible UI
+				design.
+			</p>
+			<a class="project-button" href="https://toothfairybh.com">Visit Page</a>
 		</div>
 		<div class="column">
-			<img src="./scan-face.svg" alt="right side full" />
+			<img class="project-image" src="./toothfairythumb.webp" alt="toothfairy" />
 		</div>
 	</div>
 	<div class="card">
 		<div class="column">
 			<h3>ToothFairy B2B</h3>
-			<p>Wholesale Buyer Portal</p>
-			<a href="https://rb.gy/c56mb">Visit Page</a>
-			<p>Solutions</p>
+			<p>
+				B2B platform for retailers in Bosnia built on Svelte and MedusaJS. Other technologies
+				include Redis, Postgres, Algolia search, and the Medusa admin CMS.
+			</p>
+			<a class="project-button" href="/toothfairy">Full Details</a>
 		</div>
 		<div class="column">
-			<img src="./scan-face.svg" alt="right side full" />
+			<img class="project-image" src="./tfb2bthumb.webp" alt="tf b2b" />
 		</div>
 	</div>
 	<div class="card">
 		<div class="column">
 			<h3>Jovlabs Ai</h3>
-			<p>Single Landing Page</p>
-			<a href="https://jovlabs.vercel.app">Visit Page</a>
-			<p>Solutions</p>
+			<p>
+				Landing page with authentication for hosting your own Ai chatbots. Technologies include
+				Prisma, Sql lite, Open-props and the Oobabooga api. Svelte based front-end, with chatbots
+				running on Python.
+			</p>
+			<a class="project-button" href="https://jovlabs.vercel.app">Visit Page</a>
 		</div>
 		<div class="column">
-			<img src="./scan-face.svg" alt="right side full" />
+			<img class="project-image" src="./jovlabthumb.webp" alt="jovlab ai" />
 		</div>
 	</div>
 </section>
@@ -42,24 +51,42 @@
 		margin-block: var(--size-4);
 		display: grid;
 		gap: 4rem;
-		grid-template-columns: 1fr 1fr 1fr;
+		grid-template-columns: 1fr;
 	}
 	.card {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		gap: 2rem;
+		align-items: center;
 		background-image: var(--background-2);
-		border-radius: 8px;
+		border-radius: 12px;
 		border: 2px solid var(--brand);
 		padding: var(--size-2);
 	}
 	h3 {
 		color: var(--brand);
-		text-align: center;
+		margin-block: 1rem;
 	}
 	p {
 		font-size: var(--size-4);
-		text-align: center;
+		margin-bottom: 4rem;
 	}
 	a {
 		font-size: var(--size-4);
+		color: var(--text-1);
+	}
+	.column {
+		padding: 2rem;
+	}
+	.project-button {
+		border: 2px solid var(--brand);
+		border-radius: 8px;
+		padding: 1rem 1.75rem;
+		text-decoration: none;
+	}
+	.project-image {
+		border-radius: 8px;
+		border: 2px solid var(--brand);
 	}
 
 	@media only screen and (max-width: 767px) {
@@ -68,6 +95,13 @@
 			display: grid;
 			gap: 2rem;
 			grid-template-columns: 1fr;
+		}
+		.card {
+			grid-template-columns: 1fr;
+			justify-items: center;
+		}
+		.column {
+			padding: 0.5rem;
 		}
 	}
 </style>
---
+++ src/routes/toothfairy/+page.svelte
@@ -0,0 +1,31 @@
+<h1>Tooth Fairy B2B</h1>
+<p>Screenshots</p>
+
+<div class="grid">
+	<div class="column">
+		<img class="b2b-image" src="./b2bfrontpage.webp" alt="frontpage" />
+	</div>
+	<div class="column">
+		<img class="b2b-image" src="./b2bsignup.webp" alt="registration page" />
+	</div>
+	<div class="column">
+		<img class="b2b-image" src="./b2bshop.webp" alt="shop page" />
+	</div>
+</div>
+
+<style>
+	.grid {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
+		gap: 2rem;
+		justify-items: center;
+	}
+	p {
+		margin-bottom: 4rem;
+	}
+	@media only screen and (max-width: 767px) {
+		.grid {
+			grid-template-columns: 1fr;
+		}
+	}
+</style>
